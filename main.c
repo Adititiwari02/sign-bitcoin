a20a6ca871d56d2ed6ae616848b2afa6c729bfaf
Aditi Tiwari <adititiwari@192.168.1.4>
2022-09-18 12:14:18 +0530

    Generate unsigned txn, parsed it, double hashed and DER format

--- main.c
+++ main.c
@@ -6,78 +6,29 @@
 #include "bip39.h"
 #include "curves.h"
 #include "ripemd160.h"
+#include "secp256k1.h"
 #include "sha2.h"
 
-typedef uint8_t sha2_byte;
+// typedef uint8_t sha2_byte;
+
+struct txn_struct {
+    uint8_t version[4];
+    uint8_t num_inputs;
+    uint8_t prev_txn_hash[32];
+    uint8_t prev_output_idx[4];
+    uint8_t script_len;
+    uint8_t script_sig[106];
+    uint8_t sequence[4];
+    uint8_t num_outputs;
+    uint8_t value[8];
+    uint8_t script_len2;
+    uint8_t script_pubkey[25];
+    uint8_t lock_time[4];
+};
 
 void callback(uint32_t current, uint32_t total) {}
 
-void initialize_Xprv_Xpub(uint8_t *extendedPrivateKey,
-                          uint8_t *extendedPublicKey, uint8_t depth,
-                          uint8_t child_num, HDNode *parentNode) {
-    /**
-     *
-     * 4 bytes Version (mainnet: 0488B21E public, 0488ADE4 private; testnet:
-     * 043587CF public, 04358394 private)
-     * 1 bytes Depth (0x00 for master nodes, 0x01 for level-1 derived keys, etc)
-     * 4 byte Fingerprint (first 32 bits of hash160 of parent public key)
-     * 4 byte Child Index
-     * 32 byte Chain Code
-     * 33 byte Private or Public Key byte
-     */
-
-    // version
-    extendedPrivateKey[0] = 4;
-    extendedPrivateKey[1] = 136;
-    extendedPrivateKey[2] = 178;
-    extendedPrivateKey[3] = 30;
-    extendedPublicKey[0] = extendedPrivateKey[0];
-    extendedPublicKey[1] = extendedPrivateKey[1];
-    extendedPublicKey[2] = extendedPrivateKey[2];
-    extendedPublicKey[3] = extendedPrivateKey[3];
-
-    // depth
-    extendedPrivateKey[4] = depth;
-    extendedPublicKey[4] = depth;
-
-    // fingerprint
-    uint32_t finger_print = hdnode_fingerprint(parentNode);
-    extendedPrivateKey[5] = (finger_print >> 24) & 0xFF;
-    extendedPrivateKey[6] = (finger_print >> 16) & 0xFF;
-    extendedPrivateKey[7] = (finger_print >> 8) & 0xFF;
-    extendedPrivateKey[8] = (finger_print >> 0) & 0xFF;
-    extendedPublicKey[5] = (finger_print >> 24) & 0xFF;
-    extendedPublicKey[6] = (finger_print >> 16) & 0xFF;
-    extendedPublicKey[7] = (finger_print >> 8) & 0xFF;
-    extendedPublicKey[8] = (finger_print >> 0) & 0xFF;
-
-    // child index
-    extendedPrivateKey[9] = (child_num >> 24) & 0xFF;
-    extendedPrivateKey[10] = (child_num >> 16) & 0xFF;
-    extendedPrivateKey[11] = (child_num >> 8) & 0xFF;
-    extendedPrivateKey[12] = (child_num >> 0) & 0xFF;
-    extendedPublicKey[9] = (child_num >> 24) & 0xFF;
-    extendedPublicKey[10] = (child_num >> 16) & 0xFF;
-    extendedPublicKey[11] = (child_num >> 8) & 0xFF;
-    extendedPublicKey[12] = (child_num >> 0) & 0xFF;
-
-    // chain code
-    for (int i = 0; i < 32; i++) {
-        extendedPrivateKey[13 + i] = parentNode->chain_code[i];
-    }
-    for (int i = 0; i < 32; i++) {
-        extendedPublicKey[13 + i] = parentNode->chain_code[i];
-    }
-
-    // private key / public key
-    extendedPrivateKey[45] = 0;
-    for (int i = 0; i < 32; i++) {
-        extendedPrivateKey[46 + i] = parentNode->private_key[i];
-    }
-    for (int i = 0; i < 33; i++) {
-        extendedPublicKey[45 + i] = parentNode->public_key[i];
-    }
-}
+int callback2(uint8_t by, uint8_t sig[64]) {}
 
 void getSeedAsString(char seedAsString[500], uint8_t *seed, int size) {
     unsigned char *pin = seed;
@@ -108,18 +59,93 @@ void displayInfo(HDNode *node, char *name) {
         printf("%02x", node->chain_code[i]);
     }
 }
-int main() {
-    // const char hexstring[] = "0488ADE4", *pos = hexstring;
-    // unsigned char val[4];
-    // for (size_t count = 0; count < sizeof val / sizeof *val; count++) {
-    //     sscanf(pos, "%2hhx", &val[count]);
-    //     pos += 2;
-    // }
-    // printf("0x");
-    // for (size_t count = 0; count < sizeof val / sizeof *val; count++)
-    //     printf("%d ", val[count]);
-    // printf("\n");
 
+void fill_txn_struct(struct txn_struct *txn, uint8_t *unsigned_txn_byte_array) {
+    int idx = 0;
+    for (int i = 0; i < 4; i++) {
+        txn->version[i] = unsigned_txn_byte_array[idx++];
+    }
+    txn->num_inputs = unsigned_txn_byte_array[idx++];
+    for (int i = 0; i < 32; i++) {
+        txn->prev_txn_hash[i] = unsigned_txn_byte_array[idx++];
+    }
+    for (int i = 0; i < 4; i++) {
+        txn->prev_output_idx[i] = unsigned_txn_byte_array[idx++];
+    }
+    txn->script_len = unsigned_txn_byte_array[idx++];
+    for (int i = 0; i < 25; i++) {
+        txn->script_sig[i] = unsigned_txn_byte_array[idx++];
+    }
+    for (int i = 0; i < 4; i++) {
+        txn->sequence[i] = unsigned_txn_byte_array[idx++];
+    }
+    txn->num_outputs = unsigned_txn_byte_array[idx++];
+    for (int i = 0; i < 8; i++) {
+        txn->value[i] = unsigned_txn_byte_array[idx++];
+    }
+    txn->script_len2 = unsigned_txn_byte_array[idx++];
+    for (int i = 0; i < 25; i++) {
+        txn->script_pubkey[i] = unsigned_txn_byte_array[idx++];
+    }
+    for (int i = 0; i < 4; i++) {
+        txn->lock_time[i] = unsigned_txn_byte_array[idx++];
+    }
+}
+
+void printTxnDetails(struct txn_struct *txn, int sz) {
+    printf("\nVersion: \n");
+    for (int i = 0; i < 4; i++) {
+        printf("%02x ", txn->version[i]);
+    }
+
+    printf("\nNumber of inputs: \n%02x", txn->num_inputs);
+
+    printf("\nPrevious txn hash: \n");
+    for (int i = 0; i < 32; i++) {
+        printf("%02x ", txn->prev_txn_hash[i]);
+    }
+
+    printf("\nPrevious output idx: \n");
+    for (int i = 0; i < 4; i++) {
+        printf("%02x ", txn->prev_output_idx[i]);
+    }
+
+    printf("\nScript len: \n%02x", txn->script_len);
+
+    printf("\nScript signature \n");
+    if (sz == 25) {
+        printf("(only a placeholder for now)\n");
+    }
+    for (int i = 0; i < sz; i++) {
+        printf("%02x ", txn->script_sig[i]);
+    }
+
+    printf("\nSequence: \n");
+    for (int i = 0; i < 4; i++) {
+        printf("%02x ", txn->sequence[i]);
+    }
+
+    printf("\nNumber of outputs: \n%02x", txn->num_outputs);
+
+    printf("\nValue: \n");
+    for (int i = 0; i < 8; i++) {
+        printf("%02x ", txn->value[i]);
+    }
+
+    printf("\nScript len: \n%02x", txn->script_len2);
+
+    printf("\nScript pub key: \n");
+    for (int i = 0; i < 25; i++) {
+        printf("%02x ", txn->script_pubkey[i]);
+    }
+
+    printf("\nLocktime: \n");
+    for (int i = 0; i < 4; i++) {
+        printf("%02x ", txn->lock_time[i]);
+    }
+}
+
+int main() {
     const char *mnemonic =
         "crew squeeze kid test vault razor era rotate employ remove rare fat "
         "peasant celery stable certain whale clump flush cash goat jacket wear "
@@ -175,6 +201,65 @@ int main() {
     displayInfo(out, "ADDRESS NODE");
 
     printf("\n\n");
+
+    uint8_t *private_key = out->private_key;
+    uint8_t *public_key = out->public_key;
+
+    const char unsigned_txn[] =
+        "0200000001c70de0473c83943c722823b9ead64745dff1b179a6c00120281d2f7bad3c"
+        "cebe010000001976a914059d099e62cbd89abaa71a3fd49a81982f97e8aa88acffffff"
+        "ff0160ea0000000000001976a914833e8b18093be487999c70da47abf1ad294e182788"
+        "ac00000000";
+
+    const char *pos = unsigned_txn;
+    uint8_t unsigned_txn_byte_array[110];
+    for (size_t count = 0; count < sizeof unsigned_txn_byte_array /
+                                       sizeof *unsigned_txn_byte_array;
+         count++) {
+        sscanf(pos, "%2hhx", &unsigned_txn_byte_array[count]);
+        pos += 2;
+    }
+    printf("\n\nUnsigned txn as byte array: \n");
+    for (size_t count = 0; count < sizeof unsigned_txn_byte_array /
+                                       sizeof *unsigned_txn_byte_array;
+         count++)
+        printf("%02x ", unsigned_txn_byte_array[count]);
+
+    struct txn_struct txn;
+    struct txn_struct *txn_ptr = &txn;
+    fill_txn_struct(txn_ptr, unsigned_txn_byte_array);
+    printf("\n\nTRANSACTION STRUCT: ");
+
+    printTxnDetails(txn_ptr, 25);
+
+    uint8_t shaDigest1[64], shaDigest2[64];
+    sha256_Raw(unsigned_txn_byte_array, 110, shaDigest1);
+    sha256_Raw(shaDigest1, 64, shaDigest2);
+    printf("\n\nAfter double hashing using sha256: \n");
+    for (int i = 0; i < 64; i++) {
+        printf("%02x ", shaDigest2[i]);
+    }
+
+    uint8_t sig[64];
+    int (*ptr2)(uint8_t, uint8_t[64]) = &callback2;
+    const ecdsa_curve *curve = &secp256k1;
+    x = ecdsa_sign_digest(curve, private_key, shaDigest2, sig, public_key,
+                          ptr2);
+    printf("\n\nSignature: \n");
+    for (int i = 0; i < 64; i++) {
+        printf("%02x ", sig[i]);
+    }
+
+    uint8_t der[70];
+    x = ecdsa_sig_to_der(sig, der);
+    printf("\n\nDER format: \n");
+    for (int i = 0; i < 70; i++) {
+        printf("%02x ", der[i]);
+    }
+
+    uint8_t script_sig[106];
+    printf("\n\nScript Sig format: \n");
+
     return 0;
 }
 
@@ -183,24 +268,12 @@ int main() {
 /**
  * // DERIVE PRIVATE & PUBLIC KEY
  *
- * 1. Master Node
- * 2. Master Node to Purpose Node: 44' hardened
- * 3. Purpose Node to Coin Node: 1' hardened
- * 4. Coin Node to Account Node: 0' hardened
- * 5. Account Node to Change Node: 0 non hardened
- * 6. Change Node to Address Index Node: 0 non hardened
- *
- *
- *  // GETTING THE XPUB
- *
- * 1. SHA-256 on public key
- * 2. RIPEMD-160 on result of SHA-256
- * 3. Add version byte
- * 4. Perform SHA-256 on extended RIPEMD-160
- * 5. Perform SHA-256 on previous
- * 6. Take the first 4 bytes
- * 7. 4 checksum bytes added to RIPEMD-160 from stage 4. This is the 25-byte
- * 8. binary Bitcoin Address. convert to base58 encoding
+ *      1. Master Node
+ *      2. Master Node to Purpose Node: 44' hardened
+ *      3. Purpose Node to Coin Node: 1' hardened
+ *      4. Coin Node to Account Node: 0' hardened
+ *      5. Account Node to Change Node: 0 non hardened
+ *      6. Change Node to Address Index Node: 0 non hardened
  *
  * // GENERATE UNSIGNED TRANSACTION FROM CLI TOOL.
  *
@@ -215,76 +288,28 @@ int main() {
  * // ADD THE SCRIPT SIG INSIDE THE INPUT OF THE TRANSACTION.
  **/
 
-// uint8_t digest[64];
-// for (int i = 0; i < 32; i++) {
-//     digest[i] = masterPrivateKey[i];
-// }
-// for (int i = 0; i < 32; i++) {
-//     digest[i + 32] = masterPublicKey[i];
-// }
-// printf("\n\nDigest is:\n");
-// for (int i = 0; i < 64; i++) {
-//     printf("%x", digest[i]);
-// }
-// printf("\n\nStep 1:\n");
-// uint8_t shaDigest[64];
-// sha256_Raw(masterPublicKey, 33, shaDigest);
-// for (int i = 0; i < 64; i++) {
-//     printf("%x", shaDigest[i]);
-// }
+// 30 44 02 20 1c 3b e7 1e 17 94
+// 62 1c be 3a 7a de c1 af 25 f8
+// 18 f2 38 f5 79 6d 47 15 21 37
+// eb a7 10 f2 17 4a 02 20 4f 8f
+// e6 67 b6 96 e3 00 12 ef 4e 56
+// ac 96 af b8 30 bd df fe e3 b1
+// 5d 2e 47 40 66 ab 3a a3 9b ad
 
-// printf("\n\nStep 2:\n");
-// uint8_t ripemd160Hash[20];
-// ripemd160(shaDigest, 8, ripemd160Hash);
-// for (int i = 0; i < 20; i++) {
-//     printf("%x", ripemd160Hash[i]);
-// }
-
-// printf("\n\nStep 3:\n");
-// uint8_t extendedRipemd160Hash[21];
-// extendedRipemd160Hash[0] = 0;
-// for (int i = 0; i < 21; i++) {
-//     extendedRipemd160Hash[i + 1] = ripemd160Hash[i];
-// }
-// for (int i = 0; i < 22; i++) {
-//     printf("%x", extendedRipemd160Hash[i]);
-// }
-
-// printf("\n\nStep 4:\n");
-// uint8_t len = 22;
-// uint8_t shaDigest2[64];
-// sha256_Raw(extendedRipemd160Hash, 24, shaDigest2);
-// for (int i = 0; i < 64; i++) {
-//     printf("%x", shaDigest2[i]);
-// }
-
-// printf("\n\nStep 5:\n");
-// len = 64;
-// uint8_t shaDigest3[64];
-// sha256_Raw(shaDigest2, 24, shaDigest3);
-// for (int i = 0; i < 64; i++) {
-//     printf("%x", shaDigest3[i]);
-// }
-
-// printf("\n\nStep 6:\n");
-// uint8_t addressCheckSum[4];
-// for (int i = 0; i < 4; i++) {
-//     addressCheckSum[i] = shaDigest3[i];
-// }
-// for (int i = 0; i < 4; i++) {
-//     printf("%x", addressCheckSum[i]);
-// }
-
-// printf("\n\nStep 7:\n");
-// uint8_t binaryAdd25Bytes[25];
-// for (int i = 0; i < 21; i++) {
-//     binaryAdd25Bytes[i] = extendedRipemd160Hash[i];
-// }
-// for (int i = 0; i < 4; i++) {
-//     binaryAdd25Bytes[i + 21] = addressCheckSum[i];
-// }
-// for (int i = 0; i < 25; i++) {
-//     printf("%d", binaryAdd25Bytes[i]);
-// }
-
-// printf("\n\nStep 8:\n");
+/**
+ "02 00 00 00 "                                      // version, 4
+ "01 "                                               // number of inputs, 1
+ "c7 0d e0 47 3c 83 94 3c 72 28 23 b9 ea d6 47 45 "  // prev txn hash, 32
+ "df f1 b1 79 a6 c0 01 20 28 1d 2f 7b ad 3c ce be "
+ "01 00 00 00 "                                   // prev output idx, 4
+ "19 "                                            // script length, 1
+ "76 a9 14 05 9d 09 9e 62 cb d8 9a ba a7 1a 3f "  // script sig, 106
+ "d4 9a 81 98 2f 97 e8 aa 88 ac "
+ "ff ff ff ff"               // sequence, 4
+ "01"                        // number of output, 1
+ "60 ea 00 00 00 00 00 00 "  // value, 8
+ "19"                        //  script len, 1
+ "76 a9 14 83 3e 8b 18 09 3b e4 87 99 9c 70 da 47 ab f1 ad 29 4e 18 27 "
+ "88 ac"         // script pubkey (locking script), 25
+ "00 00 00 00";  // locktime, 4
+**/
